d64f5de52136d552e6ce21df1ad125f6f74b67d3
John Hatton <hattonjd@iu.edu>
2022-09-20 17:53:47 -0400

    Fix CMake

---
+++ 01_-_Hello-World/build/src/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/dev/Saves/Cxx/Cxx_and_CMake_-_Hello_World/01_-_Hello-World/src/HelloWorld.cpp" "src/CMakeFiles/HelloWorld.dir/HelloWorld.cpp.o" "gcc" "src/CMakeFiles/HelloWorld.dir/HelloWorld.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
